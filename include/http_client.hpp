0327df4d8f161e36ca9101e024f6eb734c2c6f3e
CPPServer <126841556+cppservergit@users.noreply.github.com>
2025-06-21 19:50:24 -0400

    Add files via upload

--- include/http_client.hpp
+++ include/http_client.hpp
@@ -4,6 +4,7 @@
 #include <string_view>
 #include <map>
 #include <memory>
+#include <functional>
 
 namespace net {
 
@@ -18,11 +19,11 @@ public:
     virtual ~HttpClient() = default;
 
     [[nodiscard]] virtual HttpResponse get(std::string_view url,
-                                           const std::map<std::string, std::string>& headers = {}) const = 0;
+                                           const std::map<std::string, std::string, std::less<>>& headers = {}) const = 0;
 
     [[nodiscard]] virtual HttpResponse post(std::string_view url,
                                             std::string_view body,
-                                            const std::map<std::string, std::string>& headers = {}) const = 0;
+                                            const std::map<std::string, std::string, std::less<>>& headers = {}) const = 0;
 
     static std::unique_ptr<HttpClient> create(long connect_timeout_ms = 2000,
                                               long response_timeout_ms = 5000,
